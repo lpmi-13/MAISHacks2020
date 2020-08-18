567880985793a92f2697cada72321d8448cdefa8
cclin130 <cheng.lin103@gmail.com>
2020-08-17 20:31:06 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/McGillAISociety/MAISHacks2020/precache-manifest.eec5286595689687e60826ec7bb7edc9.js"
+  "/MAISHacks2020/precache-manifest.4e4fe68c43c903f8d5a7d61eae3b7450.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/McGillAISociety/MAISHacks2020/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MAISHacks2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
